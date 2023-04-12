c1283bf15a79166815242e0ba592e33b6241887d
katherine Buendia <kbbuendia24@gmail.com>
2023-04-12 14:53:26 -0400

    Switch practice

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("what is your favorite school subject?");
+            string answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+
+            {
+
+                case "english":
+                    Console.WriteLine("Good luck");
+                    break;
+
+                case "math":
+                    Console.WriteLine("Calculator will be your best friend");
+                    break;
+
+                case "science":
+                    Console.WriteLine("This is where the magic happens");
+                    break;
+
+                case "pe":
+                    Console.WriteLine("This is one is sooo good ! ");
+                    break;
+
+                case "lunch":
+                    Console.WriteLine("oh , this is one is my favorite too! ");
+                    break;
+
+                default:
+                    Console.WriteLine(" Your school was no fun!");
+                    break;
+
+               
+            }
+
+
         }
     }
 }
